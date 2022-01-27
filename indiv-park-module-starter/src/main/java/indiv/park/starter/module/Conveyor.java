496f2a1dde317063cf0cb2f519af95ac99c19d15
zrere <zrere@MyNoteBook>
2022-01-27 21:38:10 +0900

    master

--- indiv-park-module-starter/src/main/java/indiv/park/starter/module/Conveyor.java
+++ indiv-park-module-starter/src/main/java/indiv/park/starter/module/Conveyor.java
@@ -1,7 +1,9 @@
 package indiv.park.starter.module;
 
 import java.util.Objects;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Conveyor<P_IN> {
@@ -17,14 +19,24 @@ public class Conveyor<P_IN> {
 		return new Conveyor<P_IN>(product);
 	}
 
-	public final <P_OUT> Conveyor<P_OUT> map(Function<? super P_IN, ? extends P_OUT> mapper) {
+	public final <P_OUT> Conveyor<P_OUT> next(Function<? super P_IN, ? extends P_OUT> mapper) {
 		Objects.requireNonNull(mapper);
 		return new Conveyor<P_OUT>(mapper.apply(product));
 	}
 
-	public final <P_WITH, P_OUT> Conveyor<P_OUT> mapWith(BiFunction<? super P_IN, P_WITH, P_OUT> mapper, P_WITH with) {
+	public final <P_WITH, P_OUT> Conveyor<P_OUT> nextWith(BiFunction<? super P_IN, P_WITH, ? extends P_OUT> mapper, P_WITH with) {
 		Objects.requireNonNull(mapper);
 		return new Conveyor<P_OUT>(mapper.apply(product, with));
+	}
+	
+	public final void fin(Consumer<? super P_IN> consumer) {
+		Objects.requireNonNull(consumer);
+		consumer.accept(product);
+	}
+	
+	public final <P_WITH> void finWith(BiConsumer<? super P_IN, P_WITH> consumer, P_WITH with) {
+		Objects.requireNonNull(consumer);
+		consumer.accept(product, with);
 	}
 
 	public final P_IN publish() {
